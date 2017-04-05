ac4f91a649b608896a8227949adfdefeb64bbf56
Sean Ward <scward0@gmail.com>
2017-04-05 14:38:44 -0600

    options

--- wp-content/themes/FoundationPress-master/functions.php
+++ wp-content/themes/FoundationPress-master/functions.php
@@ -47,5 +47,11 @@ require_once( 'library/sticky-posts.php' );
 /** Configure responsive image sizes */
 require_once( 'library/responsive-images.php' );
 
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page();
+
+}
+
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/protocol-relative-theme-assets.php' );
