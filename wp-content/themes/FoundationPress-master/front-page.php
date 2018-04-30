27616a8ff2f9bcbf64ecad65d9b1ed4076d199c5
Sean Ward <scward0@gmail.com>
2018-04-30 16:19:22 -0600

    font size"

--- wp-content/themes/FoundationPress-master/front-page.php
+++ wp-content/themes/FoundationPress-master/front-page.php
@@ -33,7 +33,7 @@ $query = new WP_Query ($args);
 
 <section class="row no-max collapse" id='projects' style="padding: 0; margin: 0; background: black;">
   <br><br><br><br><br>
-  <h1 class="text-center top-headline update" style="font-size: 50px; padding-bottom: 0; color: white;">PROJECTS<br><span style="border-bottom: 1px solid white; padding: 0; margin: 0; line-height: 0; font-size:16px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></h1>
+  <h1 class="text-center top-headline update" style="font-size: 42px; padding-bottom: 0; color: white;">PROJECTS<br><span style="border-bottom: 1px solid white; padding: 0; margin: 0; line-height: 0; font-size:16px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></h1>
   <br><br><br><br><br>
   <?php if( $query->have_posts() ) : while($query->have_posts() ) : $query->the_post(); ?>
     <div class="small-12 medium-6 large-6 columns " style="padding: 0; margin: 0;">
